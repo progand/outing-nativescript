1b77c838e2e51070d43ba9ddbb26ee43b54a3528
progand <lach@ukr.net>
2017-10-25 11:47:27 +0300

    [+] Pull-to-refresh feature

--- app/pages/list/list.component.ts
+++ app/pages/list/list.component.ts
@@ -1,8 +1,11 @@
+import { screen } from "tns-core-modules/platform";
+import { Page } from "ui/page";
+import { ScrollEventData } from "ui/scroll-view";
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { Trip } from "../../shared/trip/trip";
 import { TripListService } from "../../shared/trip/trip-list.service";
-import { setTimeout } from "timer";
-import {screen} from "tns-core-modules/platform";
+import { registerElement } from "nativescript-angular/element-registry";
+registerElement("PullToRefresh", () => require("nativescript-pulltorefresh").PullToRefresh);
 
 @Component({
   selector: "list",
@@ -11,24 +14,27 @@ import {screen} from "tns-core-modules/platform";
   providers: [TripListService]
 })
 export class ListComponent implements OnInit {
+  @ViewChild("container") container: ElementRef;
   tripList: Array<Object> = [];
   isLoading = false;
   listLoaded = false;
-  imageHeight = 234*screen.mainScreen.widthDIPs/360;
-  imageStyle = `height: ${234*screen.mainScreen.widthDIPs/360}`;
+  imageHeight = 234 * screen.mainScreen.widthDIPs / 360;
+  imageStyle = `height: ${234 * screen.mainScreen.widthDIPs / 360}`;
 
-  constructor(private tripService: TripListService) { }
+  constructor(private tripService: TripListService, private page: Page) {
+    this.page.actionBar.title = "OutingTravel";
+  }
 
   ngOnInit() {
     this.refresh();
   }
 
-  refresh() {
+  refresh(silent = false) {
     this.isLoading = true;
     this.tripService.load()
       .subscribe(loadedTrips => {
         loadedTrips.forEach((tripObject) => {
-          this.tripList.unshift(tripObject);
+          this.tripList.push(tripObject);
         });
         this.isLoading = false;
         this.listLoaded = true;
@@ -42,7 +48,19 @@ export class ListComponent implements OnInit {
   dateInterval(trip: any) {
     const start = new Date(trip.dateStart);
     const end = new Date(trip.dateEnd);
-
+ 
     return `${start.toDateString()} - ${end.toDateString()}`;
+  }
+
+  refreshList(args) {
+    var pullRefresh = args.object;
+    this.tripService.load()
+      .subscribe(loadedTrips => {
+        this.tripList = [];
+        loadedTrips.forEach((tripObject) => {          
+          this.tripList.push(tripObject);
+        });
+        pullRefresh.refreshing = false;
+      });
   }
 }
